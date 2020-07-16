6974e4fbb4d53b7729ff07b84b2991804f72b7b9
Wladimir Bandeira <tecnico100@gmail.com>
2020-07-16 08:52:57 -0300

    ativando botão e construindo formulário

--- crud_notas/lib/views/note_list.dart
+++ crud_notas/lib/views/note_list.dart
@@ -1,5 +1,6 @@
 
 import 'package:crud_notas/models/note.dart';
+import 'package:crud_notas/views/note_modify.dart';
 import 'package:flutter/material.dart';
 
 class NoteList extends StatelessWidget {
@@ -25,12 +26,20 @@ class NoteList extends StatelessWidget {
     ),
   ];
 
+  String formatDateTime(DateTime dateTime) {
+    return '${dateTime.day}/${dateTime.month}/${dateTime.year}';
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Notas'),
-        actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: () {})],
+        actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => NoteModify()));
+        })],
       ),
       body: ListView.separated(
         // Poderia ser feito dessa forma ==>  separatorBuilder: (BuildContext context, int index) => Divider(height: 1, color: Colors.green),
@@ -45,7 +54,7 @@ class NoteList extends StatelessWidget {
             title: Text(notes[index].noteTitle,
             style: TextStyle(color: Theme.of(context).primaryColor),
             ),
-            subtitle: Text('20/02/2020'),
+            subtitle: Text('Últimas alterações foram em ${formatDateTime(notes[index].latestEditDateTime)}'),
           );
         },
         itemCount: notes.length,
